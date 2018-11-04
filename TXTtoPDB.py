19161cfd37808d42ed83c076260f0862b20f6c04
sja62 <44753156+sja62@users.noreply.github.com>
2018-11-04 14:01:20 -0500

    Add files via upload

---
+++ TXTtoPDB.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import glob
+os.system('txt2pdb
+#import txt2pdb_np_HP_MZ.cpp
+
+
+os.chdir("/data/B/sadams/GAG_RUNS/HP-0.20/CH0.00_N0C0/")
+
+#changes current working directory to given path
+
+for file in glob.glob("*.txt*"):
+	#figure out how to run make file on these .txt files
+	for i in ("*.txt*"):
+		print i
+		#/home/sja62/Documents/txt2pdb_np_HP_MZ.linux
+
+	print "Done"
+
+#returns files read with .txt in name
+
+#run('makefile')
+#make makefile
+#return
+
