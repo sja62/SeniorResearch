14a6790e24770da29837c11a605e8ed83ca2b3cc
sja62 <44753156+sja62@users.noreply.github.com>
2018-11-07 15:33:51 -0500

    Add files via upload

---
+++ Start.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+Script_Path18="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.00_N0C0/"
+cd $Script_Path18
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path19="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.01_N+C-/"
+cd $Script_Path19
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path1="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.01_N+C0/"
+cd $Script_Path1
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path2="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.10_N+C-/"
+cd $Script_Path2
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path3="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.10_N+C0/"
+cd $Script_Path3
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path4="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.20_N+C-/"
+cd $Script_Path4
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path5="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.20_N+C0/"
+cd $Script_Path5
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path6="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.30_N+C-/"
+cd $Script_Path6
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path7="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.30_N+C0/"
+cd $Script_Path7
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path8="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.40_N+C-/"
+cd $Script_Path8
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path9="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.40_N+C0/"
+cd $Script_Path9
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path10="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.50_N+C-/"
+cd $Script_Path10
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path11="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.50_N+C0/"
+cd $Script_Path11
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path12="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.60_N+C-/"
+cd $Script_Path12
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path13="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.60_N+C0/"
+cd $Script_Path13
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path14="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.70_N+C-/"
+cd $Script_Path14
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path15="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.70_N+C0/"
+cd $Script_Path15
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path16="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.80_N+C-/"
+cd $Script_Path16
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
+
+Script_Path17="/data/B/sadams/GAG_RUNS/HP-0.80/CH0.80_N+C0/"
+cd $Script_Path17
+
+./txt2pdb_np_HP_MZ.linux GAG.pdb  start_config.txt 32 start_config.pdb
